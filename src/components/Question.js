c2ebd1babf54ffff16d558551f5beb8db8e8395a
Hollis Crowder <hollis.crowder@gmail.com>
2024-04-04 10:17:50 -0700

    complete use effect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timeout = setTimeout(() => setTimeRemaining(timeRemaining - 1), 1000);
+
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+
+    return function cleanup() {
+      clearTimeout(timeout);
+    };
+  }, [timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
